2329d9619d7e30a13d75c8b19caa19c024c8d229
Eric B <ericabq@gmail.com>
2020-06-09 23:28:48 -0600

    add support for describing power towers and special index method

--- bowersarray_new.py
+++ bowersarray_new.py
@@ -1,3 +1,5 @@
+import math
+
 # This is another Bowers Array evaluating program I made for Jonathan Bowers' birthday!
 # And like the previous one, I will probably be updating it with new features in the following days,
 # so it can run longer and take more shortcuts before it starts decrimenting an incredibly huge number by one each time enter is pressed.
@@ -11,6 +13,7 @@ class Bowersarray:
     def __init__(self, input_a, input_a_num = 0):
         self.a = input_a
         self.a_num = input_a_num
+        self.canonize()
 
     def __str__(self):
         string = '{'
@@ -36,81 +39,105 @@ class Bowersarray:
         while len(self.a) > 0 and self.a[-1] == []:
             del self.a[-1]
 
+    # get element by index. index is a list of coordinates describing position in the row, plane, realm, etc.
+    def at_index(self, pos_list):
+        # check if index does not refer to element in first plane
+        # since the type currently only supports 2D arrays, the result is 1
+        if pos_list[2:] != [0] * len(pos_list[2:]):
+            return 1
+        if len(pos_list) < 2:
+            pos_list += [0] * (2 - len(pos_list))
+        subarray = self.a
+        for i in [1, 0]:
+            if pos_list[i] >= len(subarray):
+                return 1
+            subarray = subarray[pos_list[i]]
+        return subarray
+
     # evaluates the array and (theoretically) returns the final value
     # precondition: array is canonized
     # postcondition: destroys the original array in the process; final array has only two entries
     def eval(self):
-        # empty array: return 1
-        if len(self.a) == 0:
-            self.print_status('= %d' % 1)
-            raw_input()
-            return 1
-
         # default prime: evaluate to base
-        if len(self.a[0]) < 2 or self.a[0][1] == 1:
-            self.print_status('= %d' % self.a[0][0])
+        if self.at_index([1]) == 1:
+            self.print_status('= {}'.format(self.at_index([0])))
             raw_input()
-            return self.a[0][0]
+            return self.at_index([0])
 
         # catastrophic rule: 
-        while len(self.a) > 1 or len(self.a[0]) > 2:
+        while not (len(self.a) == 1 and len(self.a[0]) <= 2):
             self.print_status()
             raw_input()
             self.catastrophic_step()
         # at this point, the array is only two entries
-        print 'A%d = ' % self.a_num + self.__str__() + ' = %d^%d' % (self.a[0][0], self.a[0][1])
+        b = self.at_index([0])
+        p = self.at_index([1])
+        result = None
+        # if the power is less than 10^10000, print its digits
+        # if the power is less than 10^10000, return the power as an integer and not a special exponential type
+        # otherwise, just return a string describing the exponential expression
+        if type(p) == int and p * math.log10(b) < 10000:
+            result = b ** p
+            print 'A{} = '.format(self.a_num) + self.__str__() + ' = {}^{} = {}'.format(b, p, result)
+        else:
+            result = str(b) + '^' + str(p)
+            print 'A{} = '.format(self.a_num) + self.__str__() + ' = {}^{}'.format(b, p)
         raw_input()
-        return self.a[0][0] ** self.a[0][1]
+        return result
 
     # perform the catastrophic step of Bowers array evaluation
     # precondition: self.a has at least one non-1 entry after the first two, is trimmed of excess 1's and empty rows, and has a prime entry unequal to 1 (in other words, catastrophic rule applies)
     # postcondition: self.a is still trimmed of excess 1's and empty rows
     def catastrophic_step(self):
-        # find the row index (index) and the column index (pilotindex) of the pilot
-        for index, sub in enumerate(self.a):
-            if index == 0:
+        # find the row index (pilotrow) and the column index (pilotindex) of the pilot
+        for pilotrow, sub in enumerate(self.a):
+            if pilotrow == 0:
                 sub = sub[2:]
             if len(sub) != 0:
                 break
-            index += 1
+            pilotrow += 1
 
-        if index == 0:
+        if pilotrow == 0:
             pilotindex = find_pilot(sub) + 2
         else:
             pilotindex = find_pilot(sub)
 
-        # Make new copy of array a and decrement prime entry
+        # Make new copy of array a and decrement prime entry (unless prime entry is stored as a very large power instead of an integer)
         new = Bowersarray(copy_list(self.a), self.a_num + 1)
-        new.a[0][1] -= 1
+        if type(new.at_index([1])) == int:
+            new.a[0][1] -= 1
         new.canonize()
 
         # decrement pilot of a (this may uncanonize a, but it will be canonized later)
-        self.a[index][pilotindex] -= 1
+        # again, only if pilot is actually stored as an integer
+        if type(self.at_index([pilotindex, pilotrow])) == int:
+            self.a[pilotrow][pilotindex] -= 1
 
         # set the passengers on previous rows equal to the base
-        base = self.a[0][0]
-        prime = self.a[0][1]
-        for i in xrange(0, index):
+        base = self.at_index([0])
+        prime = self.at_index([1])
+        print("what is base: {}".format(base))
+        for i in xrange(0, pilotrow):
             self.a[i] = [base] * prime
         # set all passengers on the same row as the pilot equal to the base
         for i in xrange(pilotindex - 1):
-            self.a[index][i] = self.a[0][0]
+            self.a[pilotrow][i] = base
 
         if pilotindex > 0:
             # if the copilot exists, make it a reference to the unevaluated array new
-            self.a[index][pilotindex - 1] = 'A%d' % new.a_num
+            self.a[pilotrow][pilotindex - 1] = 'A{}'.format(new.a_num)
             self.canonize()
             self.print_status()
             raw_input()
 
             # after evaluating new, set copilot equal to that value
-            self.a[index][pilotindex - 1] = new.eval()
+            self.a[pilotrow][pilotindex - 1] = new.eval()
         else:
             self.canonize()
 
     # print the array and number of array with an optional message after the array
     def print_status(self, msg = ''):
-        print 'A%d =' % self.a_num, self, msg
+        print 'A{} ='.format(self.a_num), self, msg
 
 def array_string(a, a_num):
     string = '{'
@@ -148,26 +175,24 @@ def copy_list(a):
 print "Welcome to Eric's ALL-NEW Array Manager! This program will evaluate Bowers arrays even better than before!"
 
 a = []
-sub = []
 
-entry = int(raw_input("Let's get started!\nEnter the first member of your array, A0\n(or 0 to go to the next row, or a non-positive number to finish): "))
+print "Your array will be called A0."
+row_text = raw_input("Let's get started!\nEnter the first row of A0 (separated by spaces), or press enter to finish: ")
 
-while entry >= 0:
+while len(row_text) > 0:
     # entry a single row of the array
-    while entry > 0:
-        sub.append(entry)
-        entry = int(raw_input("Enter the next member of A0, 0 to go to the next row, or a non-positive number to finish: "))
+    row = map(int, row_text.strip().split(' '))
+    if not all([n > 0 for n in row]):
+        print "A Bowers array cannot contain numbers less than 1."
+        exit()
 
     # append row to array
-    a.append(sub)
-    if entry < 0:
-        break
+    a.append(row)
 
-    sub = []
-    entry = int(raw_input("Enter the next member of A0, 0 to go to the next row, or a non-positive number to finish: "))
+    row_text = raw_input("Enter the next row of A0 (separated by spaces), or press enter to finish: ")
 
+# Create Bowers array object (automatically internally canonized by removing ones)
 arr = Bowersarray(a)
-arr.canonize()
 
 print "Your array is:", arr
 print "Now it's time to EVALUATE your array!!! After each step, press enter to go to the next one!"
